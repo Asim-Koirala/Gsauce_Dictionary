6e9ec26a172cb11525930e0a444d2c5760ba8679
Asim Koirala <asimkoirala2006@gmail.com>
2024-06-15 14:22:25 +0545

    Fixed some errors due to update in font awesome and dictionary API

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let searchBar = document.querySelector('#search'),
-searchIcon = document.querySelector('.fas.fa-search'),
-crossIcon = document.querySelector('.fas.fa-times'),
+searchIcon = document.querySelector('.fa-magnifying-glass'),
+crossIcon = document.querySelector('.fa-xmark'),
 info = document.querySelector('.info'),
 Meaningspan = info.querySelector('ul li:nth-child(2) span'),
 wordh3 = info.querySelector('ul li:first-child h3'),
 wordPronounciation = info.querySelector('ul li:first-child span'),
 exampleSpan = info.querySelector('ul li:nth-child(3) span'),
 synonymsList = info.querySelector('ul li:last-child .li'),
-infoSPAN = document.querySelector('.wrapper > span');
+infoSPAN = document.querySelector('.wrapper > span'), 
+synonymsExist = false;
 
 crossIcon.onclick = ()=>{
     searchBar.value = '';
@@ -31,14 +32,17 @@ function data(information, word){
     synonyms = [],
     partOfSpeech = information[0].meanings[0].partOfSpeech,
     pronounciation = information[0].phonetic;
+
+    console.log(information[0])
     for(var i = 0; i<5; i++){
-        if(information[0].meanings[0].definitions[0].synonyms[i] !== undefined){
-            synonyms.push(information[0].meanings[0].definitions[0].synonyms[i]);
+        if(information[0].meanings[0].synonyms[i] !== undefined){
+            synonyms.push(information[0].meanings[0].synonyms[i]);
         }else{
             synonyms.push('');
         }
         
     }
+
     Meaningspan.textContent = definition;
     wordPronounciation.textContent = `${partOfSpeech}/${pronounciation}`;
     wordh3.textContent = searchWord;
@@ -47,9 +51,12 @@ function data(information, word){
     synonymsList.innerHTML = '';
     for(var i = 0; i<5; i++){
         if(synonyms[i] != ''){
+            synonymsExist = true;
             synonymsList.innerHTML+= `<span onclick="search(this)">${synonyms[i]}</span>`
         }else{
-            synonymsList.innerHTML = `<span>Sorry, no synonyms found.</span>`
+            if(!synonymsExist){
+                synonymsList.innerHTML = `<span>Sorry, no synonyms found.</span>`
+            }
         }
         
     }
@@ -58,7 +65,9 @@ function data(information, word){
 
 function fetchData(word){
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-    fetch(url).then(res=>res.json()).then(info => data(info, word));
+    fetch(url).then(res=>res.json()).then(info =>{
+        data(info, word);
+    } );
 }
 
 searchBar.addEventListener('keyup', (e)=>{
